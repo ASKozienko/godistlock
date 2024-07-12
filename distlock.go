d8445391b7b1e55577abcf01549593f577be3461
Alexander Kozienko <a.s.kozienko@gmail.com>
2024-07-12 11:46:02 +0300

    distlock with prefix

--- distlock.go
+++ distlock.go
@@ -45,26 +45,26 @@ func (s *distLock) UnlockWithTimeout(session, id string, timeout time.Duration) 
 }
 
 type distLockWithPrefix struct {
-	distLock
+	DistLock
 	prefix string
 }
 
-func NewWithPrefix(l DistLockBase, prefix string) DistLock {
-	return &distLockWithPrefix{distLock{l}, prefix}
+func NewWithPrefix(l DistLock, prefix string) DistLock {
+	return &distLockWithPrefix{l, prefix}
 }
 
 func (d *distLockWithPrefix) Lock(ctx context.Context, session, id string, blocking bool) (bool, error) {
-	return d.distLock.Lock(ctx, session, d.prefix+id, blocking)
+	return d.DistLock.Lock(ctx, session, d.prefix+id, blocking)
 }
 
 func (d *distLockWithPrefix) Unlock(ctx context.Context, session, id string) (bool, error) {
-	return d.distLock.Unlock(ctx, session, d.prefix+id)
+	return d.DistLock.Unlock(ctx, session, d.prefix+id)
 }
 
 func (d *distLockWithPrefix) LockWithTimeout(ctx context.Context, session, id string, blocking bool, timeout time.Duration) (bool, error) {
-	return d.distLock.LockWithTimeout(ctx, session, d.prefix+id, blocking, timeout)
+	return d.DistLock.LockWithTimeout(ctx, session, d.prefix+id, blocking, timeout)
 }
 
 func (d *distLockWithPrefix) UnlockWithTimeout(session, id string, timeout time.Duration) (bool, error) {
-	return d.distLock.UnlockWithTimeout(session, d.prefix+id, timeout)
+	return d.DistLock.UnlockWithTimeout(session, d.prefix+id, timeout)
 }
